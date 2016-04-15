b072e3cee288647a8b5111a3356dae483928eeb2
Mohamed Shahawy <mohamed@gibbins.local>
2016-04-15 12:44:18 +0200

    Initial Commit

---
+++ ARM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ARM
+//
+//  Created by Mohamed Shahawy on 4/15/16.
+//  Copyright © 2016 Mohamed Shahawy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
